618c42881f1430e6fe7d09cabed41a68ccc11b5c
anniehau <anniehaurani@gmail.com>
2021-10-25 16:28:19 -0300

    Requisito 3 completo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
+        <Link data-testid="shopping-cart-button" to="/Cart">
+          Carrinho de Compras
+        </Link>
         <p
           data-testid="home-initial-message"
         >
