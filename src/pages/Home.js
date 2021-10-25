e419e10a3f500f1d51524e426eb4e7645bbb0dc9
Wilian Pereira <wilian_pereira92@outlook.com>
2021-10-25 16:56:08 -0300

    Adicionado input e o button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      query: '',
+      categoryId: '',
+    };
+  }
+
   render() {
+    const { query, categoryId } = this.state;
     return (
       <div>
         <p
@@ -9,6 +18,14 @@ class Home extends React.Component {
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <div>
+          <form onSubmit>
+            <label htmlFor="query-input">
+              <input data-testid="query-input" type="text" value={ query } />
+            </label>
+            <button data-testid="query-button" type="submit">Buscar</button>
+          </form>
+        </div>
       </div>
     );
   }
