7bf6109dbaf09d65fbe518b2b6743061e831b92e
anniehau <anniehaurani@gmail.com>
2021-10-25 21:32:15 -0300

    Arruma erro de lint do import

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import * as api from '../services/api';
 import Card from '../components/Card';
-import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   constructor() {
